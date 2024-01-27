75982bda039808b3d1d3cc4fe61ba875402063b7
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-01-27 16:12:25 +0200

    Add pthread/FindMinMax initial project

---
+++ pthread/FindMinMax/FindMinMax/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FindMinMax
+//
+//  Created by Volodymyr Myroniuk on 27.01.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
