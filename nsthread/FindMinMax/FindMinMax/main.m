d656bace512695fc7994a335233d4921518a42bd
Volodymyr Myroniuk <vladimironiuk@gmail.com>
2024-02-03 08:02:30 +0200

    Add initial project

---
+++ nsthread/FindMinMax/FindMinMax/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FindMinMax
+//
+//  Created by Volodymyr Myroniuk on 03.02.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
